4ffb32432583b86e4643779add16e89e31a6b8b1
pkolinko <58145506+pkolinko@users.noreply.github.com>
2021-01-25 01:19:23 -0800

    Add files via upload
    fixed some notes

--- index.js
+++ index.js
@@ -8,24 +8,27 @@
 // 
 //     1.) Use google chrome
 //     2.) Log into https://familyinfocenter.brighthorizons.com or similar website
-
+//
 //     3.) The pictures are not actually stored at https://familyinfocenter.brighthorizons.com , but rather
 //         at https://mybrightday.brighthorizons.com , so switch your view from familyinfocenter to mybrightday
 //         as described in the next few steps: 
-
+//
 //     4.) Open "Dev Tools" in chrome
 //          *Windows: ctrl+shift+c
 //          *Mac: Command+Option+C
 //          *nix: ???
-
+//
 //     5.) From familyinfocenter click on one of the photos of your children to see a larger photo of your child pop up within familyinfocenter website.
 //         Use "Dev Tools" "Elements" tab to locate the full URL of this photo. You can search for HTML tag that starts with <img class = 
 //         it will have a source, src = "/remote/v1 ... " etc..
-//         If you don't do the steps described here
+//         If you don't do the steps described here, you will get a CORS error.
+//
 //     6.) In Dev Tools, right click on the URL and copy link address. Open a new Chrome tab and copy paste the link into the address bar. 
 //         You should now see the large photo of your child by itself, not embedded into the familyinfocenter browser. 
-//         Double check that the address bar starts with https://mybrightday.brighthorizons.com 
+//         Double check that the address bar starts with https://mybrightday.brighthorizons.com
+// 
 //     7.) Paste this script in console
+//
 //     8.) Hit "Enter"
 //
 //
